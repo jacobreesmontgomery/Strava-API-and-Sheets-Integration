83d2c01227f2327e7843cd84b4cb7a1e290e751c
Jacob Montgomery <jrm260@pitt.edu>
2023-04-04 22:23:03 -0400

    got past the initial barrier, now it's just jogging my memory about using pandas for ML and going through the process of data setup

--- python/goons_sheet_analytics.py
+++ python/goons_sheet_analytics.py
@@ -1,16 +1,22 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-
+import pandas
 
 # use creds to create a client to interact with the Google Drive API
-scope = ['https://spreadsheets.google.com/feeds']
+scope = ['https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name('python\client_secret.json', scope)
 client = gspread.authorize(creds)
 
-# Find a workbook by name and open the first sheet
-# Make sure you use the right name here.
-sheet = client.open("Strava API and Sheets Integration using Apps Script").sheet1
+# Opening the Strava API sheet
+sheet = client.open("Strava API and Sheets Integration using Apps Script")
 
-# Extract and print all of the values
-list_of_hashes = sheet.get_all_records()
-print(list_of_hashes)
+# Acquiring the "GOONS" sheet
+goons_sheet = sheet.worksheet("GOONS")
+
+# Acquiring the activities from the "GOONS" sheet and converting to a column-formatted dataframe
+activities = goons_sheet.get_values("A2:P1000")
+activities_df = pandas.DataFrame(activities, 
+                                columns=goons_sheet.get_values("A1:P1"))
+print(activities_df.dtypes) # Printing the dataframe types
+
+
