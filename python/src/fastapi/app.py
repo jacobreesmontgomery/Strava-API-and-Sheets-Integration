303a93133665f3714be5245e433def2cce4e1c31
reesmonty <jacobreesmontgomery@gmail.com>
2025-01-18 13:46:14 -0500

    fixing sumn

--- python/src/fastapi/app.py
+++ python/src/fastapi/app.py
@@ -21,8 +21,8 @@ logger = SimpleLogger(log_level="INFO", class_name=__name__).logger
 package_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.insert(0, package_path)
 from datasetup.api.strava_api import StravaAuthorization, StravaAPI
-from code.dao.strava_athlete_dao import StravaAthleteDao
-from code.dao.strava_activities_dao import StravaActivitiesDao
+from src.dao.strava_athlete_dao import StravaAthleteDao
+from src.dao.strava_activities_dao import StravaActivitiesDao
 from python.src.dao.services.database_service import DatabaseService
 
 # Load environment variables
