3cea840ed43cb3ac748773fe2a0d12976274ce1b
reesmonty <jacobreesmontgomery@gmail.com>
2025-01-19 10:12:58 -0500

    ugh

--- python/src/fast_api/app.py
+++ python/src/fast_api/app.py
@@ -7,20 +7,9 @@ OVERVIEW: This file will drive the front-end webpage.
 # IMPORTS
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
+# TODO - JACOB: Fix this shit so it actually works... dumb python bs
 from fast_api import activities_router
-from os import environ
-from dotenv import load_dotenv
-
-from utilities.simple_logger import SimpleLogger
-
-logger = SimpleLogger(log_level="INFO", class_name=__name__).logger
-
-# Load environment variables
-load_dotenv()
-CLIENT_ID = environ.get("CLIENT_ID")
-CLIENT_SECRET = environ.get("CLIENT_SECRET")
-REDIRECT_URI = environ.get("REDIRECT_URI")
-AUTH_EXCHANGE_LINK = environ.get("AUTH_EXCHANGE_LINK")
 
 app = FastAPI(
     title="API Documentation",
